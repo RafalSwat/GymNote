eaf10b1dd8d46cae8b533c8e154f8fd7bdc84e91
RafalSwat <rafal.swat@gmail.com>
2020-03-09 19:44:06 +0100

    Changed look of "AddUserExerciseView". (conceptual action remained unchanged)

---
+++ GymNote/AddUserExerciseView.swift
@@ -0,0 +1,69 @@
+//
+//  AddUserExerciseView.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 09/03/2020.
+//  Copyright © 2020 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddUserExerciseView: View {
+    
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    @State var exerciseName = ""
+    @State var isCheck = true
+    @Binding var showAddView: Bool
+    @Binding var list: [Exercise]
+    
+    var body: some View {
+        VStack {
+            
+            Text("Enter the name of your own, new exercise to the list: ")
+                .multilineTextAlignment(.center)
+                .lineLimit(2).fixedSize(horizontal: false, vertical: true)
+                .padding(.vertical, 5)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(Color.secondary)
+                .font(.headline)
+            
+            
+            HStack {
+                TextField("exercise name", text: $exerciseName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button(action: {
+                    if self.exerciseName != "" {
+                        withAnimation {
+                            let newExercise = Exercise(name: self.exerciseName, isCheck: self.isCheck)
+                            self.list.insert(newExercise, at: self.list.startIndex)
+                            self.exerciseName = ""
+                            self.showAddView = false
+                            
+                        }
+                    } else {
+                        withAnimation {
+                            self.showAddView = false
+                            
+                        }
+                    }
+                }) {
+                    Image(systemName: "arrowshape.turn.up.right.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(.secondary)
+                }
+            }.padding()
+        }.background(Color.magnesium)
+    }
+    
+    
+}
+
+struct AddUserExerciseView_Previews: PreviewProvider {
+    
+    @State static var prevList = [Exercise]()
+    @State static var prevShowAddView = true
+    
+    static var previews: some View {
+        AddUserExerciseView(showAddView: $prevShowAddView, list: $prevList)
+    }
+}
