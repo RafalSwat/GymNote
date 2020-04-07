83ec53636e0efb36931aca7e008ed64df2731cad
RafalSwat <rafal.swat@gmail.com>
2020-04-07 16:08:08 +0200

    Conformed "Training" class to "Hashable" protocol. Implemented two requires methods.

--- GymNote/Model/Training.swift
+++ GymNote/Model/Training.swift
@@ -10,8 +10,8 @@ import Foundation
 import Combine
 import SwiftUI
 
-class Training {
-    
+class Training: Hashable {
+
     var trainingID = UUID().uuidString
     var trainingName: String
     var trainingSubscription: String
@@ -29,7 +29,16 @@ class Training {
         self.listOfExercises = exercises
     }
     
+    //MARK: String is a built-in type, which means it is hashable by default
+    func hash(into hasher: inout Hasher) {
+        return hasher.combine(trainingName)
+    }
     
+    //MARK: Hashable inherits from Equatable so our struct must implemented requirements for both
+    //We must implemented func that compare two var, to chceck if there are equal or not (Equatable protocol)
+    public static func == (lhs: Training, rhs: Training) -> Bool {
+        return lhs.trainingName == rhs.trainingName
+    }
     
     
     
