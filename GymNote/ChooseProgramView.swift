c2c571036ce14a73fdf43538de09c87b323aff0f
RafalSwat <rafal.swat@gmail.com>
2020-04-10 11:37:58 +0200

    Created a list that display a training names, and setup preview to see how it looks in canvas.

--- GymNote/ChooseProgramView.swift
+++ GymNote/ChooseProgramView.swift
@@ -12,11 +12,19 @@ struct ChooseProgramView: View {
     
     @EnvironmentObject var session: FireBaseSession
     @State private var passageCreateProgram = false
+    @State var listOfTrainings: [Training]
     
     var body: some View {
         VStack {
+            List {
+                ForEach(listOfTrainings, id: \.trainingID) { training in
+                    Text(training.trainingName)
+                }
+            }
+            
             AddButton(addButtonText: "create new program",
-                      action: {print("AddButton in ChooseProramView tapped!")}, addingMode: self.$passageCreateProgram)
+                      action: {print("AddButton in ChooseProramView tapped!")},
+                      addingMode: self.$passageCreateProgram)
                 .padding()
         }
         .navigationBarTitle("Choose Program", displayMode: .inline)
@@ -25,11 +33,15 @@ struct ChooseProgramView: View {
 
 struct ChooseProgramView_Previews: PreviewProvider {
     
-    @State static var session = FireBaseSession()
+    @State static var prevSession = FireBaseSession()
+    @State static var prevListOfTrainings = [Training(name: "My Training",
+                                                      subscription: "My litte subscription",
+                                                      date: "01-01-2020",
+                                                      exercises: [Exercise(name: "My Exercise")])]
     
     static var previews: some View {
         NavigationView {
-            ChooseProgramView().environmentObject(session)
+            ChooseProgramView(listOfTrainings: prevListOfTrainings).environmentObject(prevSession)
         }
     }
 }
