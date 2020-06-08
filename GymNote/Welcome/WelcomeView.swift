2d7e9e5600b9917ad168731cb14615ffcb6d4e3e
RafalSwat <rafal.swat@gmail.com>
2020-06-08 18:03:39 +0200

    Added new binding property: "alreadySignIn" to WelcomeView and shared its value with displaying views. Removed NavigationView from WelcomeView.

---
+++ GymNote/Welcome/WelcomeView.swift
@@ -0,0 +1,49 @@
+//
+//  WelcomeView.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 04/12/2019.
+//  Copyright © 2019 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    
+    //MARK: Properties
+    @EnvironmentObject var session: FireBaseSession
+    @State private var isRegistered = true
+    @Binding var alreadySignIn: Bool
+    
+    //MARK: View
+    var body: some View {
+        KeyboardHost {
+            VStack {
+                
+                Image("staticImage")
+                
+                ColorfulPicker(selection: $isRegistered)
+                
+                if isRegistered {
+                    LoginView(alreadySignIn: $alreadySignIn)
+                } else {
+                    SignupView(alreadySignIn: $alreadySignIn)
+                }
+            }.padding()
+        }
+    }
+    
+}
+
+
+struct WelcomeView_Previews: PreviewProvider {
+    
+    @State static var prevAlreadySignIn = false
+    
+    static var previews: some View {
+        NavigationView {
+            WelcomeView(alreadySignIn: $prevAlreadySignIn)
+                .environmentObject(FireBaseSession())
+        }
+    }
+}
