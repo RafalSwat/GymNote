b17c5b3992b5f648fab5faa697f845f408a51ff9
RafalSwat <rafal.swat@gmail.com>
2020-06-09 15:08:42 +0200

    Added new view to display Note i edit mode.

---
+++ GymNote/Note/EditNoteView.swift
@@ -0,0 +1,21 @@
+//
+//  EditNoteView.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 08/06/2020.
+//  Copyright © 2020 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditNoteView: View {
+    var body: some View {
+        Text("Edit Note View...")
+    }
+}
+
+struct EditNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditNoteView()
+    }
+}
