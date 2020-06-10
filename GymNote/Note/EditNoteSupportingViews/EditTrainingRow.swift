6ad519b8da0e68cf19d571957828ae69ce299459
RafalSwat <rafal.swat@gmail.com>
2020-06-10 11:20:29 +0200

    Added new view that will be responsible for displaying rows of a list in EditNoteView. (Rows contains two buttons: delete and details)

---
+++ GymNote/Note/EditNoteSupportingViews/EditTrainingRow.swift
@@ -0,0 +1,50 @@
+//
+//  EditTrainingRow.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 10/06/2020.
+//  Copyright © 2020 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditTrainingRow: View {
+    
+    @Binding var training: Training
+    
+    var body: some View {
+        HStack(spacing: 15) {
+            Button(action: {
+                print("Delete Action!")
+            }) {
+                Image(systemName: "trash")
+                    .font(.title)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(training.trainingName)
+                Text(training.trainingSubscription)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            
+            Button(action: {
+                print("Go To Details Action!")
+            }) {
+                Image(systemName: "chevron.right")
+                    .font(.title)
+            }
+            
+        }
+    }
+}
+
+struct EditTrainingRow_Previews: PreviewProvider {
+    
+    @State static var prevTraining = Training()
+    
+    static var previews: some View {
+        EditTrainingRow(training: $prevTraining)
+    }
+}
