db543ca448eaea006af9eb67cd5d52489d6252e5
RafalSwat <rafal.swat@gmail.com>
2020-06-20 23:25:46 +0200

    Added new view EditTrainingView that will be responsible for editing training. (HostView - strategy).

---
+++ GymNote/Training/EditTrainingView.swift
@@ -0,0 +1,64 @@
+//
+//  EditTrainingView.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 20/06/2020.
+//  Copyright © 2020 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditTrainingView: View {
+    
+    @Binding var training: Training
+    @State var addMode = false
+    @State var selectedExercises = [Exercise]()
+    
+    var body: some View {
+        VStack {
+            List {
+                Section(header: Text("Basic info")) {
+                    VStack {
+                        HStack {
+                            Text("Training name:")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Spacer()
+                            TextField("Enter your name: ", text: $training.trainingName)
+                                .font(.title)
+                                
+                        }
+                        HStack {
+                            Text("Subscription:")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Spacer()
+                            TextField("Enter your name: ", text: $training.trainingSubscription)
+                        }
+                    }
+                }
+                Section(header: Text("List of exercises")) {
+                    ForEach(training.listOfExercises, id: \.self) { exercise in
+                        Text(exercise.exerciseName)
+                    }
+                }
+            }
+            AddButton(addingMode: $addMode)
+                .padding()
+                .sheet(isPresented: $addMode) {
+                        ExercisesListView(finishTyping: self.$addMode, selectedExercises: self.$selectedExercises)
+                }
+        }
+    }
+}
+
+struct EditTrainingView_Previews: PreviewProvider {
+    
+    @State static var prevTraining = Training()
+    
+    static var previews: some View {
+        NavigationView {
+            EditTrainingView(training: $prevTraining)
+        }
+    }
+}
