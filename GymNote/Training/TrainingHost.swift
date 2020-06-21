3710ae58b7cd2a0d2bbd68f74515329336d81623
RafalSwat <rafal.swat@gmail.com>
2020-06-21 11:41:45 +0200

    Added new host view: TrainingHost. Training will we displaying in two modes (normal and edit).

---
+++ GymNote/Training/TrainingHost.swift
@@ -0,0 +1,50 @@
+//
+//  TrainingHost.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 21/06/2020.
+//  Copyright © 2020 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct TrainingHost: View {
+    
+    @State var editMode = false
+    @EnvironmentObject var session: FireBaseSession
+    @Binding var training: Training
+    @State var draftTraining = Training()
+    
+    
+    var body: some View {
+        VStack {
+            if editMode {
+                EditTrainingView(training: $draftTraining)
+                    .onAppear {
+                        self.draftTraining = self.training
+                }
+            } else {
+                TrainingView(training: training)
+            }
+        }
+        .navigationBarItems(
+            leading: CancelEditModeButton(editMode: $editMode, cancelAction: {
+                print("cancel action!")
+            }),
+            trailing: EditModeButton(editMode: $editMode, editAction: {
+                print("edit action!")
+            }))
+        
+    }
+}
+
+struct TrainingHost_Previews: PreviewProvider {
+    
+    @State static var prevTraining = Training()
+    
+    static var previews: some View {
+        NavigationView {
+            TrainingHost(training: $prevTraining)
+        }
+    }
+}
