5d7ee2bc9c31e36b43398e7102dfc4f8495ed04b
RafalSwat <rafal.swat@gmail.com>
2020-06-21 11:34:35 +0200

    Moved SetSeriesView to EditTrainingSupportingViews group and the appearance has changed slightly. (functionality the stay the same)

---
+++ GymNote/Training/EditTrainingSupportingViews/SetSeriesView.swift
@@ -0,0 +1,63 @@
+//
+//  SetSeriesView.swift
+//  GymNote
+//
+//  Created by Rafał Swat on 31/03/2020.
+//  Copyright © 2020 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct SetSeriesView: View {
+    
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    @Binding var exercise: Exercise // binding from list of selected 
+    @State var series = 1
+    
+    var body: some View {
+        HStack {
+            
+            Text("number of series:")
+                .font(.callout)
+                .foregroundColor(.secondary)
+            Text(String(series))
+            Spacer()
+            
+            Button(action: {
+                self.series += 1
+                self.exercise.exerciseNumberOfSerises = self.series
+            }) {
+                Image(systemName: "plus.square")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.orange)
+                    .padding(.horizontal)
+            }
+            
+            Button(action: {
+                if self.series >= 2 {
+                    self.series -= 1
+                    self.exercise.exerciseNumberOfSerises = self.series
+                }
+            }) {
+                Image(systemName: "minus.square")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.red)
+            }
+            
+            
+        }
+        .onAppear() {
+            self.series = self.exercise.exerciseNumberOfSerises
+        }
+        
+    }
+}
+
+struct SetSeriesView_Previews: PreviewProvider {
+    
+    @State static var prevTempExercise = Exercise(name: "example1")
+    
+    static var previews: some View {
+        SetSeriesView(exercise: $prevTempExercise)
+    }
+}
