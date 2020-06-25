2f5e51da19e8f845f38eb5852cb07447a6c3e84b
RafalSwat <rafal.swat@gmail.com>
2020-06-25 21:21:56 +0200

    Added button and implemented its action. Button will be responsible for setup user stats with exercise data. Embedded everything in KeyboardHost.

--- GymNote/Training/TrainingView.swift
+++ GymNote/Training/TrainingView.swift
@@ -13,47 +13,60 @@ struct TrainingView: View {
     @EnvironmentObject var session: FireBaseSession
     var training: Training
     let dataString = DateConverter.dateFormat.string(from: Date())
+    @State var conform = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            List {
-                Section(header: Text("")) {
-                    
-                    VStack(alignment: .leading) {
-                        Text(training.trainingName)
-                            .font(.largeTitle)
-                            .padding(.horizontal)
-                            .padding(.bottom)
+        KeyboardHost {
+            VStack(alignment: .leading) {
+                List {
+                    Section(header: Text("")) {
                         
-                        Text(training.trainingSubscription)
-                            .padding(.horizontal)
-                        Text("last training: \(dataString)")
-                            .padding(.horizontal)
-                            .font(.caption)
-                            .foregroundColor(.secondary)
+                        VStack(alignment: .leading) {
+                            Text(training.trainingName)
+                                .font(.largeTitle)
+                                .padding(.horizontal)
+                                .padding(.bottom)
+                            
+                            Text(training.trainingSubscription)
+                                .padding(.horizontal)
+                            Text("last training: \(dataString)")
+                                .padding(.horizontal)
+                                .font(.caption)
+                                .foregroundColor(.secondary)
+                        }
+                        .padding()
+                        
                     }
-                    .padding()
-                    
-                }
-                Section(header: Text("")) {
-                    ForEach(training.listOfExercises, id: \.self) { exercise in
-                        ExerciseView(exercise: exercise)
+                    Section(header: Text("")) {
+                        ForEach(training.listOfExercises, id: \.self) { exercise in
+                            ExerciseView(exercise: exercise, conform: self.$conform)
+                        }
                     }
-                }
-                Section(header: Text("")) {
-                    VStack {
-                        Button(action: {
-                            print("Some Action")
-                        }) {
-                            HStack {
-                                Image(systemName: "checkmark.circle")
-                                    .font(.largeTitle)
-                                Spacer()
-                                Text("Confirm finished training")
-                                Spacer()
-                            }.padding()
-                        }.buttonStyle(RectangularButtonStyle())
-                            .padding(.vertical)
+                    Section(header: Text("")) {
+                        VStack {
+                            Button(action: {
+                                
+                                self.conform = true
+                                
+                                let userID = self.session.userSession!.userTrainings.userID
+                                let currentDate = DateConverter.dateFormat.string(from: Date())
+                                
+                                for exercise in self.training.listOfExercises {
+                                    self.session.userSession!.userStats.insert(exercise, at: self.session.userSession!.userStats.startIndex)
+                                    self.session.addExerciseStatstoFBR(id: userID, date: currentDate, exercise: exercise)
+                                }
+                                
+                            }) {
+                                HStack {
+                                    Image(systemName: "checkmark.circle")
+                                        .font(.largeTitle)
+                                    Spacer()
+                                    Text("Confirm finished training")
+                                    Spacer()
+                                }.padding()
+                            }.buttonStyle(RectangularButtonStyle())
+                                .padding(.vertical)
+                        }
                     }
                 }
                 .listStyle(PlainListStyle())
