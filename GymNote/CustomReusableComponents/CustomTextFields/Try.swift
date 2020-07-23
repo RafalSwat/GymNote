3b8491764b9ce2a6a1813064ebf74854b56ce8e3
RafalSwat <rafal.swat@gmail.com>
2020-07-23 14:58:10 +0200

    Changed CustomTextFields to decimalTextFields inside Try.

--- GymNote/CustomReusableComponents/CustomTextFields/Try.swift
+++ GymNote/CustomReusableComponents/CustomTextFields/Try.swift
@@ -14,31 +14,40 @@ struct Try: View {
     var weights: Binding<[String]>
     var index: Int
     
-    @State var reps: String
-    @State var weight: String
+    @Binding var reps: Double
+    @Binding var weight: Double
+    
+    var decimalFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }
+    
     
     var body: some View {
         HStack {
-            CustomTextfield(text: self.$reps, keyType: .decimalPad, placeHolder: "  reps")
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+            DecimalTextField("reps",
+                             value: self.$reps)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 30, maxHeight: 60)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color(red: 0.7, green: 0.7, blue: 0.7), lineWidth: 1)
+                    )
+            DecimalTextField("weight",
+                             value: self.$weight,
+                             formatter: self.decimalFormatter)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 30, maxHeight: 60)
                 .overlay(
                     RoundedRectangle(cornerRadius: 6)
                         .stroke(Color(red: 0.7, green: 0.7, blue: 0.7), lineWidth: 1)
                 )
-
-            CustomTextfield(text: self.$reps, keyType: .decimalPad, placeHolder: "  weight")
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 6)
-                        .stroke(Color(red: 0.7, green: 0.7, blue: 0.7), lineWidth: 1)
-
-            )
+            
         }
     }
     
     func action() {
-        self.repeats.wrappedValue[self.index] = self.reps
-        self.weights.wrappedValue[self.index] = self.weight
+        self.repeats.wrappedValue[self.index] = String(self.reps)
+        self.weights.wrappedValue[self.index] = String(self.weight)
     }
 }
 
@@ -47,10 +56,10 @@ struct Try_Previews: PreviewProvider {
     
     @State static var prevArray = ["1"]
     static var prevIndex = 0
-    @State static var prevText = ""
+    @State static var prevText = 12.0
     
     
     static var previews: some View {
-        Try(repeats: $prevArray, weights: $prevArray, index: prevIndex, reps: prevText, weight: prevText)
+        Try(repeats: $prevArray, weights: $prevArray, index: prevIndex, reps: $prevText, weight: $prevText)
     }
 }
