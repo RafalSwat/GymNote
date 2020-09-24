5d1e748b72b80e67183d0c75c50a05cc1369731f
RafalSwat <rafal.swat@gmail.com>
2020-09-24 21:13:28 +0200

    Solved problems with pickers inside ProfileEditView.

--- GymNote/Profile/ProfileEditView.swift
+++ GymNote/Profile/ProfileEditView.swift
@@ -18,10 +18,10 @@ struct ProfileEditView: View {
     @State var doneUpdating = false
     @State var doneChangingPhoto = false
     
-    //Array only for pick the height and gender of a user
+    //Array only for pick the height of a user
     var userPossibleHeight = Array(40...250)
-    var userPossibleGender = ["non", "male", "female"]
-    @State var selectedGender = 0
+    @State var selectedGender = Gender.non
+    
     
     var body: some View {
         
@@ -64,27 +64,34 @@ struct ProfileEditView: View {
                             TextField("Enter your surname: ", text: $profile.userSurname)
                         }}
                     Section(header: Text("Height")) {
+                        
+                        
                         Picker(
                             selection: $profile.userHeight,
-                            label: Text("")) {
-                                ForEach(0 ..< userPossibleHeight.count) {
-                                    //FIXME: "-40" because for unknown reasons, the picker chooses a number that is exactly 40 different
-                                    Text("\(self.userPossibleHeight[$0] - 40) cm")
-                                }
-                        }.pickerStyle(WheelPickerStyle())
+                            label: EmptyView()) {
+                            
+                            ForEach(self.userPossibleHeight, id: \.self) {
+                                Text("\($0) cm")
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        .labelsHidden()
+                        .frame(width: UIScreen.main.bounds.width/1.11)
+                        .clipped()
+                        
+                        
                     }
                     Section(header: Text("Gender")) {
                         Picker(
-                            selection: $selectedGender,
-                            label: Text("")) {
-                                ForEach(0 ..< userPossibleGender.count) {             Text("\(self.userPossibleGender[$0])")
-                                }
-                        }
-                            //FIXME: the solution is unsatisfactory because it doesn't react on "swipe", only for clicking so that without clicking the gender doesn't change ad all
-                            .onTapGesture {
-                                self.profile.userGender = self.userPossibleGender[self.selectedGender]
+                            selection: self.$profile.userGender,
+                            label: EmptyView()) {
+                            
+                            ForEach(Gender.allCases, id: \.self) {
+                                Text("\($0.rawValue)").tag($0.rawValue)
+                            }
                         }
                     }
+                    
                     .pickerStyle(SegmentedPickerStyle())
                     .padding(.vertical)
                     
@@ -93,7 +100,8 @@ struct ProfileEditView: View {
                             selection: $profile.userDateOfBirth,
                             in: ...Date(),
                             displayedComponents: .date) {
-                                Text("")
+                            
+                            Text("")
                         }
                     }
                 }.listStyle(GroupedListStyle())
@@ -110,12 +118,12 @@ struct ProfileEditView: View {
     }
     
     func setupGender() {
-        if self.profile.userGender == "non" {
-            self.selectedGender = 0
+        if self.profile.userGender == "female" {
+            self.selectedGender = Gender.female
         } else if self.profile.userGender == "male" {
-            self.selectedGender = 1
+            self.selectedGender = Gender.male
         } else {
-            self.selectedGender = 2
+            self.selectedGender = Gender.non
         }
     }
 }
@@ -130,5 +138,11 @@ struct ProfileEditView_Previews: PreviewProvider {
             ProfileEditView(profile: $prevProfile)
         }
     }
+}
+
+enum Gender: String, CaseIterable {
+    case non
+    case male
+    case female
 }
 
